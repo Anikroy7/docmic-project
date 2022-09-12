26796ef1f28e6215716cc7de7b83f5833e9f3761
Anikroy7 <roy645800@gmail.com>
2022-09-12 18:54:46 +0600

    navbar section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App sm:bg-red-400 md:bg-lime-400 lg:bg-amber-400">
-      <h2>This is docmic project</h2>
-      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
-
+    <div>
+      <Home></Home>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,jsx}"],
   theme: {
+    extend: {},
     screens: {
       'sm': '100px',
       // => @media (min-width: 100px) { ... }
@@ -12,6 +13,32 @@ module.exports = {
       'lg': '992px',
       // => @media (min-width: 1440px) { ... }
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#642DFF",
+
+          "secondary": "#2D89FF",
+
+          "accent": "#37CDBE",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#F7F7F7",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 
